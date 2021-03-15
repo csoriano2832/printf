f920dc2baefcc23b6e8f25531621a5a31059e114
csoriano2832 <2832@holbertonschool.com>
2021-03-15 15:36:32 +0000

    Add print_numbers.c

---
+++ more_conversion_types.c
@@ -0,0 +1,71 @@
+#include "holberton.h"
+
+/**
+  * print_digit - prints an integer
+  * @ap: argument
+  *
+  * Return: count of numbers printed
+  */
+
+int print_digits(va_list ap)
+{
+	int num = 0, count = 0;
+
+	num = va_arg(ap, int);
+	count += print_int(num);
+
+	return (count);
+}
+
+/**
+ * print_unsigned - printes unsigned interger
+ * @ap: argument
+ *
+ * Return: count for numbers printed
+ */
+
+int print_unsigned(va_list ap)
+{
+	unsigned int num = 0;
+	int count = 0;
+
+	num = va_arg(ap, unsigned int);
+	count += recur_unsigned(num);
+
+	return (count);
+}
+
+/**
+ * print_binary - prints binary numbers
+ * @ap: arguments
+ *
+ * Return: count for numbers printed
+ */
+
+int print_binary(va_list ap)
+{
+	int num = 0, count = 0;
+
+	num = va_arg(ap, int);
+	count += recur_binary(num);
+
+	return (count);
+}
+
+/**
+ * print_octal - prints octal numbers
+ * @ap: argument
+ *
+ * Return: return count for numbers printed
+ */
+
+int print_octal(va_list ap)
+{
+	unsigned int num = 0;
+	int count = 0;
+
+	num = va_arg(ap, unsigned int);
+	count += recur_octal(num);
+
+	return (count);
+}
---
+++ print_numbers.c
@@ -0,0 +1,84 @@
+#include "holberton.h"
+
+/**
+ * print_int - prints an integer
+ * @n: number to be printed
+ *
+ * Return: count of numbers printed
+ */
+int print_int(int n)
+{
+	int count = 1, num = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		num = n * -1;
+	}
+
+	else
+	{
+		num = n;
+	}
+
+	if (num / 10)
+	{
+		count += print_int(num / 10);
+	}
+	_putchar((num % 10) + 48);
+	return (count);
+}
+
+/**
+ * recur_unsigned - prints unsigned integer
+ * @n: number to be printed
+ *
+ * Return: count of numbers
+ */
+int recur_unsigned(unsigned int n)
+{
+	int count = 1;
+
+	if ((n / 10) > 0)
+	{
+		count += recur_unsigned(n / 10);
+	}
+	_putchar((n % 10) + 48);
+	return (count);
+}
+/**
+ * recur_binary - prints a number in binary form
+ * @n: number to be printed
+ *
+ * Return: count of numbers printed
+ */
+int recur_binary(int n)
+{
+	int count = 1;
+
+	if ((n / 2) > 0)
+	{
+		count += recur_binary(n / 2);
+	}
+	_putchar((n % 2) + 48);
+	return (count);
+}
+
+/**
+ * recur_octal - prints an integer in octal form
+ * @n: number to be printed
+ *
+ * Return: count of numbers printed
+ */
+int recur_octal(unsigned int n)
+{
+	int count = 1;
+
+	if ((n / 8) > 0)
+	{
+		count += recur_octal(n / 8);
+	}
+	_putchar((n % 8) + 48);
+	return (count);
+}
