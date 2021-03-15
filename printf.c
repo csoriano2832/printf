851516bdf84d6a6b07143f4cfcb860a6b0fbdd31
csoriano2832 <2832@holbertonschool.com>
2021-03-15 15:03:30 +0000

    Add printf.c file

---
+++ printf.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _printf - function to print
+ * @format: string to print
+ *
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int num_of_chars = 0;
+
+	if (format == NULL)
+		exit(1);
+
+	va_start(ap, format);
+
+	num_of_chars = conv_spec(format, ap);
+	
+	va_end(ap);
+	return (num_of_chars);
+}
