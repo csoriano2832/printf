c8555339e0a35f7c2813899cc0962861dcf1a01d
roquesrolando <roquesrolando@hotmail.com>
2021-03-16 20:26:59 +0000

    added hexadecimals

--- print_hex.c
+++ print_hex.c
@@ -51,7 +51,29 @@ int recur_h(unsigned int n)
 	{
 		count += recur_h(n / 16);
 	}
-	_putchar((n % 16) + 48);
+	switch(n % 16)
+	{
+		case 10:
+			_putchar('a');
+			break;
+		case 11:
+			_putchar('b');
+			break;
+		case 12:
+			_putchar('c');
+			break;
+		case 13:
+			_putchar('d');
+			break;
+		case 14:
+			_putchar('e');
+			break;
+		case 15:
+			_putchar('f');
+			break;
+		default:
+			_putchar((n % 16) + 48);
+	}
 	return (count);
 }
 
